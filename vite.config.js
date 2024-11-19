891711c95498c0274472393cdc114b764eb74a3a
UoABioLab <xingye.cheng@auckland.ac.nz>
2024-11-20 04:16:20 +1300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   server: {
-    host: true,
+    host: '0.0.0.0',  // 允许外部访问，比 true 更明确
     port: 9203
   },
-  base: './'
+  base: '/FruitCutting/',  // 替换成你的 GitHub 仓库名
 })
